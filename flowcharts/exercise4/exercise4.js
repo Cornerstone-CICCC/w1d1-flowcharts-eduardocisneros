98be8b6410611b9fdd832b89195785714f2ccd7e
EduardoCisneros <eduardo.cisnerosm@hotmail.com>
2024-05-14 21:09:08 -0700

    Exercise #4

---
+++ flowcharts/exercise4/exercise4.js
@@ -0,0 +1,10 @@
+//-----------Exercise 4-----------//
+
+function exercise4(b, c, d) {
+    if ((b + c) > d) {
+        console.log("Possible")
+    } else {
+        console.log("Not Possible")
+    }
+};
+exercise4(10, 1, 4);
