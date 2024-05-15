7e83a275377a86990c4098aad46b9de2eac2e301
EduardoCisneros <eduardo.cisnerosm@hotmail.com>
2024-05-14 21:07:48 -0700

    Exercise #3

---
+++ flowcharts/exercise3/exercise3.js
@@ -0,0 +1,12 @@
+//-----------Exercise 3-----------//
+
+function exercise3(a, b, c) {
+    if (a > b && a > c) {
+        console.log(a)
+    } else if (b > a && b > c) {
+        console.log(b)
+    } else if (c > a && c > b) {
+        console.log(c)
+    }
+};
+exercise3(1, 30, 5);
